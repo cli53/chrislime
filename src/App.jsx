d2cae3d47e76cba679f47dfa74197c55f72d4ed3
Chris <licmike75@gmail.com>
2017-12-18 21:05:03 -0500

    webpack ready for test

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import reader from 'react-dom';
+
+class App extends Component {
+  render() {
+    <h1>Hello World</h1>
+  }
+};
+
+export default App;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,32 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
 const path = require('path');
 
 module.exports = {
-  entry: 
+  entry: path.join(__dirname, '../src', 'index.jsx'),
+  output: { 
+    path: path.resolve(__dirname, '../', 'public'),
+    filename: 'bundle.js',
+    },
+  module : {
+    rules: [
+        {
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      },
+      {
+      test: /\.css$/,
+      loader: ['style-loader','css-loader']
+      }
+    ]  
+  },
+  plugins: [
+    HtmlWebpackPluginConfig
+  ],
 }
