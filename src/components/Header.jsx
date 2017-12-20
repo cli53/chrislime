f526ac1f0376068a9302468858aa2cb1cacc8f5b
Chris <licmike75@gmail.com>
2017-12-20 12:02:55 -0500

    added png logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Nav() {
     <React.Fragment>
     <ul id='nav-contain'>
       <li className='nav' key={1}>
-        <NavLink className='nav' exact={true} to='/'>Logo</NavLink>
+        <NavLink className='nav' exact={true} to='/'><img id='logo' src='../../images/logo.png' alt ='logo'/></NavLink>
       </li>
       <li className='nav' key={2}>
         <NavLink className='nav' exact={true} to='/About'>About</NavLink>
