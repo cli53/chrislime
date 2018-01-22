3aeb555d1ed2060c59e21de304156572932a7300
Chris <licmike75@gmail.com>
2018-01-21 23:52:01 -0500

    animations added and basic home page complete for web

--- src/components/home/body/Body.jsx
+++ src/components/home/body/Body.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 import Bio from './Bio.jsx'
 import Projects from '../projects/Projects.jsx';
 import Login from './Login.jsx';
 import ErrorBoundary from '../../ErrorBoundary.jsx';
 import Email from './Email.jsx';
 
-const Body = () => {
-  return (
+const Body = () => (
     <React.Fragment>
     <div id='mid-sec'>
+    <Fade left>
     <Bio/>
+    </Fade>
     </div>
     <article>
     <ErrorBoundary>
@@ -18,7 +20,6 @@ const Body = () => {
     </article>
     <Login/>
     </React.Fragment>
-)
-};
+);
 
 export default Body;
