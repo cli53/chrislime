15fd3fce1c0c1d7d5638793f3cab58230b35b114
Chris <licmike75@gmail.com>
2018-02-01 15:48:07 -0500

    updated TODO list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,17 @@ export default function Nav() {
     <React.Fragment>
       <ul id="nav-contain">
         <li className="nav" key={1}>
-          <NavLink className="nav" exact={true} to='/'><img className='img' id='logo' src={logo} alt ='logo' title='Chris Li'/></NavLink>
-      </li>
-      <li className='nav' key={2}>
-        <NavLink className='nav' id='About' exact={true} to='/About'>About</NavLink>
-      </li>
-    </ul>
+          <NavLink className="nav" to="/">
+            <img className="img" id="logo" src={logo} alt="logo" title="Chris Li" />
+          </NavLink>
+        </li>
+        <li className="nav" key={2}>
+          <NavLink className="nav" id="About" to="/About">About</NavLink>
+        </li>
+      </ul>
     </React.Fragment>
-  )
-};
+  );
+}
 
 //Webpack you need to require images in order for Webpack to process them, which would explain why external images load while internal do not, so instead of <img src={"/images/resto.png"} /> you need to use <img src={require('/images/image-name.png')} /> replacing image-name.png w/ the correct image for each of them. 
 //That way Webpack is able to process and replace the source img.
--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 // BrowserRouter does is it allows React Router to pass the app’s routing information down to any child component it needs (via context).
 // When the app’s location matches a certain path, Route will render a specified component, when it doesn’t, it will render null.
 import Header from './Header.jsx'
-import App from '../App.jsx';
 import About from './about/About.jsx';
 import Home from './home/Home.jsx';
 import Secret from './Secret.jsx';
@@ -17,9 +16,9 @@ export default function routes({ giphyArr, giphyCall, contactVis, contactBar, gi
   <Switch>
   {/* When React Router renders a component, it passes that component three things: match, location, and history. */}
   {/* When the app’s location matches a certain path, Route will render a specified component, when it doesn’t, it will render null.  */}
-    <Route path='/About' render={()=> <About giphyArr={giphyArr} giphyCall={giphyCall} giphyChange={giphyChange}/>} />
-    <Route path='/Secret' render={()=> <Secret/>}/>
-    <Route exact path='/' render={()=> <Home/>} />    
+    <Route path="/About" render={()=> <About giphyArr={giphyArr} giphyCall={giphyCall} giphyChange={giphyChange} />} />
+    <Route path="/Secret" render={()=> <Secret />} />
+    <Route exact path="/" render={()=> <Home />} />
   </Switch>
   </main>
   <Footer contactVis={contactVis} contactBar={contactBar}/>
