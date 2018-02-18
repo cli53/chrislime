a74d65fda9b6a90f1cd0aa96f1aa623fe86f4734
Chris <cli53@binghamton.edu>
2018-02-17 20:44:39 -0500

    added test env to fix import issue with jest.config

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import expect from 'expect';
 import sinon from 'sinon';
-import { shallow } from 'enzyme';
+import { shallow, render, mount } from 'enzyme';
 import Header from '../src/components/Header.jsx';
 import App from '../src/App.jsx';
 // The shallow function in Enzyme does a shallow rendering to the DOM. Shallow rendering does not render any components nested within the Welcome component.
 
 describe('React unit tests', () => {
 
-  describle('<Header/>', () => {
-    it('renders Header Component', () => {
-      const component = shallow(<Header/>);
-    })
-  })
+  // describle('<Header/>', () => {
+  //   it('renders Header Component', () => {
+  //     const wrapper = shallow(<Header/>);
+  //   })
+  // })
 
   describe('<App/>', () => {
     it('renders 1 <App/> component', () => {
-      const component = shallow(<App/>)
-      expect(component).to.have.length(1);
+      const wrapper = shallow(<App/>)
+      const appState = wrapper.state().contactVisible
+      expect(appState).toEqual(false);
     })
   }) 
 
---
+++ tests/jest.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+// import Header from '../src/components/Header.jsx';
+
+// describe('Header', () => {
+//   it('Check if About renders the text "About"', () => {
+//     const component = renderer.create(<Header/>);
+//     const json = component.toJSON();
+//     expect(json).toMatchSnapshot();
+//   })
+// })
