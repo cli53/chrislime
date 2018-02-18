3fa7d560bacee10fb94ce6e19fd2a1e0416a18db
Chris <cli53@binghamton.edu>
2018-02-17 21:30:01 -0500

    modified config for testing

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,16 +3,11 @@ import expect from 'expect';
 import sinon from 'sinon';
 import { shallow, render, mount } from 'enzyme';
 import Header from '../src/components/Header.jsx';
+import Footer from '../src/components/Footer.jsx';
 import App from '../src/App.jsx';
 // The shallow function in Enzyme does a shallow rendering to the DOM. Shallow rendering does not render any components nested within the Welcome component.
 
 describe('React unit tests', () => {
-
-  // describle('<Header/>', () => {
-  //   it('renders Header Component', () => {
-  //     const wrapper = shallow(<Header/>);
-  //   })
-  // })
 
   describe('<App/>', () => {
     it('renders 1 <App/> component', () => {
@@ -20,6 +15,5 @@ describe('React unit tests', () => {
       const appState = wrapper.state().contactVisible
       expect(appState).toEqual(false);
     })
-  }) 
-
+  })
 })
